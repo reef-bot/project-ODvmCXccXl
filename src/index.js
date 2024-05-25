d68f4b220bd5db04028c93aca871c16a1164a9f6
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 22:46:10 +0530

    Added: `src/index.js`

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// index.js
+
+const Discord = require('discord.js');
+require('dotenv').config();
+const fs = require('fs');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./src/commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+const { prefix } = require('./config.json');
+
+client.once('ready', () => {
+  console.log('Bot is ready');
+});
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  if (!client.commands.has(commandName)) return;
+
+  const command = client.commands.get(commandName);
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error trying to execute that command!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
